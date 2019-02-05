1a0d2e5ca40441d9e54206ab556c2849ad8ca18e
bruno aguilar <josebrunoaguilar@gmail.com>
2019-02-04 18:20:33 -0700

    Actualice y aumente la carpeta sockets

---
+++ sockets/sockets.ts
@@ -0,0 +1,8 @@
+import { Socket } from "socket.io";
+
+
+export const deconectar = (cliente: Socket)=>{
+    cliente.on('disconnect', ()=>{
+        console.log('Cliente desconectado');
+    });
+};
